c4898c2fb8fde209c411f3ce76851bdaa6e827b8
VinSjo <vincentsjogren@gmail.com>
2021-12-31 13:44:41 +0100

    testing

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(`header a[href="${currentPage}"]`)
 	.attr('title', 'You are here');
 
 $('button.icon.burger').on('pointerdown', function (ev) {
-	$('.animation-caption').text(ev.type);
+	// $('.animation-caption').text(ev.type);
 	$('nav').toggleClass('open');
 });
 
@@ -12,6 +12,11 @@ $('button.icon.burger').on('pointerdown', function (ev) {
  * "Closes" burger-menu when window is resized to more than 720px in width,
  * and hides initial transition-animation when width is less
  */
+
+$(window).on('pointerdown', function (ev) {
+	$('.animation-caption').text($(ev.target).get(0).nodeName);
+	console.log($(ev.target).get(0).nodeName);
+});
 window
 	.matchMedia('only screen and (min-width: 768px)')
 	.addEventListener('change', function (ev) {
