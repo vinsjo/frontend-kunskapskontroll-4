87a2d171cca6f03c1d53d2e417a0f71dc7601541
VinSjo <vincentsjogren@gmail.com>
2021-12-31 14:18:07 +0100

    hopefully fixed touch issues

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const currentPage = location.pathname.split('/').at(-1) || 'index.html';
+const path = location.pathname.split('/').filter(str => str.length);
+const currentPage = path[path.length - 1] || 'index.html';
+
 $(`header a[href="${currentPage}"]`)
 	.addClass('current')
 	.attr('title', 'You are here');
 
 $('button.icon.burger').on('pointerdown', function (ev) {
-	// $('.animation-caption').text(ev.type);
 	$('nav').toggleClass('open');
 });
 
@@ -12,11 +13,6 @@ $('button.icon.burger').on('pointerdown', function (ev) {
  * "Closes" burger-menu when window is resized to more than 720px in width,
  * and hides initial transition-animation when width is less
  */
-
-$(window).on('touchstart', function (ev) {
-	$('.animation-caption').text($(ev.target).get(0).nodeName);
-	console.log($(ev.target).get(0).nodeName);
-});
 window
 	.matchMedia('only screen and (min-width: 768px)')
 	.addEventListener('change', function (ev) {
